d53f8aaac43c41df1e1fa2f07926664df4c24f46
Siddharth Bhat <siddharth.bath@tweag.io>
2019-06-22 15:20:28 +0200

    yes, implementation now works

--- presburger.hs
+++ presburger.hs
@@ -5,11 +5,59 @@ import qualified Data.Set as S
 import Data.Bits
 
 data NFA a where
-    NFA :: Ord s => S.Set s -- ^ universe: su
+    NFA :: (Ord s, Show s)  => S.Set s -- ^ universe: su
       -> (S.Set s) -- ^ initial: si
       -> (S.Set s) -- ^ final: sf
       -> (a -> s -> S.Set s) -- ^ transition: t
       -> NFA a
+
+data DFA a where
+    DFA :: (Ord s, Show s) => S.Set s -- ^ universe: su
+     -> (S.Set s)  -- ^ initial: si
+     -> (S.Set s)  -- ^ final: sf
+     -> (a -> s -> s)  -- ^ transition: t
+     -> DFA a
+
+-- | Directly make the DFA work on the powerset space of the NFA
+nfa2dfa :: NFA a -> DFA a
+nfa2dfa (NFA su si sf t) = DFA su' (S.singleton si) sf' t' where
+    su' = S.powerSet su
+    t' = nondet t
+    -- | Any subset of the powerset that has a final state
+    -- is a final state
+    sf' = S.filter (intersects sf) su'
+
+
+-- | Trivially embed a DFA as an NFA
+dfa2nfa :: DFA a -> NFA a
+dfa2nfa (DFA su si sf t) = NFA su si sf t'
+  where t' = \a s -> S.singleton $ t a s
+
+
+-- | Remove unreachable states from the DFA for a fixed
+-- input alphabet
+dfaRemoveUnreachable :: S.Set a -- ^ input alphabet
+                     -> DFA a
+                     -> DFA a
+dfaRemoveUnreachable as dfa@(DFA su si sf t) =
+    let next ss = (foldMap (\s -> (S.map (`t` s) as)) ss) `S.union` ss
+        reached = transitiveClosure next si
+        unreached = su S.\\ reached
+    in (DFA su si (sf S.\\ unreached) t)
+
+-- | Minimal dfa
+dfaMinimal :: DFA a -> DFA a
+dfaMinimal = undefined
+
+
+-- | nfa reversal
+-- To transition, apply the transition map on the universe,
+-- and keep all the sets that reach the current set. This is
+-- effectively a reverse of the NFA transition, since we now return
+-- all sets that _enter_ the current set.
+reverse :: NFA a -> NFA a
+reverse (NFA su si sf t) =
+    NFA su sf si $ \a s -> S.filter (S.member s . t a) su
 
 -- | NFA that accepts all states
 univ :: NFA a
@@ -19,7 +67,15 @@ univ = let su = S.singleton () in NFA su su su (\ _ _ -> su)
 empty :: NFA a
 empty = let su = S.singleton () in NFA su su S.empty (\ _ _ -> su)
 
+nfaStateSpaceCard :: NFA a -> Int
+nfaStateSpaceCard (NFA su _ _ _) = S.size su
 
+
+debugNFA :: NFA a -> IO ()
+debugNFA (NFA su si sf t)= do
+    putStrLn $ "univ: " <> show su
+    putStrLn $ "initial: " <> show su
+    putStrLn $ "final: " <> show su
 
 -- | Disjoint union of two sets
 disjointUnion :: (Ord a, Ord b) =>
@@ -45,9 +101,16 @@ nfaIntersection (NFA su si sf t) (NFA su' si' sf' t') =
       (S.cartesianProduct sf sf')
       (\a (s, s') -> S.cartesianProduct (t a s) (t' a s'))
 
--- | Accepts complement of two languages
+-- | Complement of DFA
+dfaComplement :: DFA a -> DFA a
+dfaComplement (DFA su si sf t) = DFA su si (su S.\\ sf) t
+
+
+-- | Accepts complement of a language
+-- Come up with an intuitive explanation for why just flipping
+-- the final states does *not* cut it for an NFA
 nfaComplement :: NFA a -> NFA a
-nfaComplement (NFA su si sf t) = NFA  su si (su S.\\ sf) t
+nfaComplement = dfa2nfa . dfaComplement . nfa2dfa
 
 -- | Perform a non-deterministic step of the NFA
 nondet :: Ord s => (a -> s -> S.Set s) -> a -> S.Set s -> S.Set s
@@ -56,27 +119,37 @@ nondet f a s = foldMap (f a) s
 
 
 -- | Run multiple steps of the NFA
-nondets :: Ord s => Foldable g  => (a -> s -> S.Set s) -> g a -> S.Set s -> S.Set s
-nondets f aa ss = foldr (nondet f) ss aa
+nondetSequence :: Ord s => Foldable g  => (a -> s -> S.Set s) -> g a -> S.Set s -> S.Set s
+nondetSequence f aa ss = foldr (nondet f) ss aa
+
 
 -- | Check if two sets have non-empty intersection
 intersects :: Ord s => S.Set s -> S.Set s -> Bool
 intersects xs ys = not $ S.null $ S.intersection xs ys
 
--- | Return set of values reachable (this includes interior nodes)
--- from transition relation and initial state
--- given input stream
-visited :: (Foldable f, Ord s) => (a -> s -> S.Set s) -> f a -> S.Set s -> S.Set s
-visited d aa = fixSet $ \s -> (nondets d aa s) `S.union` s where
-  fixSet f a
-    | a == a'   = a
-    | otherwise = fixSet f a'
-    where a' = f a
-{-# inline visited #-}
+
+transitiveClosure :: (Eq s, Ord s) =>
+    (S.Set s -> S.Set s) -> (S.Set s ->  S.Set s)
+transitiveClosure f a =
+    let a' = f a
+     in if a == a' then a else (transitiveClosure f a') `S.union` a
+
+
+-- | All possible states visited from given alphabet set
+-- and initial states
+visitedAlphabet :: (Ord s, Ord a) =>
+    (a -> s -> S.Set s) -- ^ transition relation
+    -> S.Set a -- ^ alphabet
+    -> S.Set s -- ^ Initial state
+    -> S.Set s -- ^ all reachable states
+visitedAlphabet t as =
+  -- | All possible transitions from all possible sets
+  let next ss = foldMap (\s -> foldMap (`t` s) as) ss
+  in transitiveClosure $ \ss -> next ss `S.union` ss
 
 -- | Check whether NFA accepts
 runNFA :: NFA a -> [a] -> Bool
-runNFA (NFA _ si sf t) aa = intersects (nondets t aa si) sf
+runNFA (NFA _ si sf t) aa = intersects (nondetSequence t aa si) sf
 
 -- | Now encode PA as NFAs
 
@@ -144,26 +217,23 @@ eq i k = let
    in NFA su su su $ \v _ ->
         if ((v .!. i) == (v .!. k)) then su else S.empty
 
--- | Create a list containing bit vectors that have the
--- given var bit both on and off
-enableDisableBit :: BV ->  Var -> [BV]
-enableDisableBit v i = [setBit v i, clearBit v i]
-
 -- | Create an NFA for:  exists x. P x
 -- find all states reachable from the state where i is enabled
 -- (and also from 0, why??)
 exists :: Var -> NFA BV -> NFA BV
 exists i (NFA su si sf t) =
-  let si' = visited t (BV 0 `enableDisableBit` i) si
-      t' v s =
-          foldMap (\curv -> t curv s)
-                  (v `enableDisableBit` i)
+  let si' = visitedAlphabet t (S.fromList [BV 0, BV (bit i)]) si
+      t' v s = t v s `S.union` t (setBit v i) s
   in NFA su si' sf t'
 
 -- | Create an NFA for: forall x. P x
 -- Encode this as not (exists x. not (P x))
 forall :: Var -> NFA BV -> NFA BV
 forall i = nfaComplement . exists i . nfaComplement
+
+-- | Check if NFA language accepts the empty string
+acceptsEmpty :: NFA a -> Bool
+acceptsEmpty (NFA su si sf t) = intersects si sf
 
 
 main :: IO ()
